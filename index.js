e4de30d26f9a4c2f51dc7918f66d7d5ee31fee51
SaillyKevin <75009579+SaillyKevin@users.noreply.github.com>
2022-03-04 01:19:26 +0100

    Added a basic Bot startup
    Token will be regenerated when this will be made public
    Still need to add events and everything else

---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+/*############################################*/
+/* Homemade modules */
+/*############################################*/
+
+
+
+/*############################################*/
+/* Imported modules */
+/*############################################*/
+const Discord = require('discord.js');
+
+
+/*############################################*/
+/* Discord Client creation */
+/*############################################*/
+
+const discordClient = new Discord.Client({
+  shards: 'auto',
+  restRequestTimeout: 1000,
+  restGlobalRateLimit: 50,
+  intents:[
+    //Check https://discord.js.org/#/docs/discord.js/stable/class/Intents?scrollTo=s-FLAGS
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MEMBERS,
+    Discord.Intents.FLAGS.GUILD_BANS,
+    //Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    //Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+    Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+    Discord.Intents.FLAGS.GUILD_INVITES,
+    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+    Discord.Intents.FLAGS.GUILD_PRESENCES,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Discord.Intents.FLAGS.DIRECT_MESSAGES,
+    Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Discord.Intents.FLAGS.GUILD_SCHEDULED_EVENTS
+  ],
+  rejectOnRateLimit: ()=>{return(true)},
+  sweepers:
+  {
+      'messages':{
+        interval: 60,
+        lifetime: 3600 //A message is saved in cache for an hour
+      }
+  }
+});
+
+
+
+/*############################################*/
+/* Starting the bot */
+/*############################################*/
+
+discordClient.login('OTAzMzI0NjM1MTA4NjM0NjU0.YXrUnw.h15i_YmNlT2X-pl3plEgTJA-Bxs').then(()=>{console.log('Bot prêt !');});
