76ebeef6ecf009ac8edd1fe33f1d03cf506ac2d7
LJ50 <75009579+LJ5O@users.noreply.github.com>
2022-11-28 22:42:42 +0100

    We now check if replies are ephemeral in database

--- modules/event_functions.js
+++ modules/event_functions.js
@@ -47,7 +47,7 @@ module.exports = {
     logger.debug("Custom slash command "+interaction.commandName+" ran in server "+interaction.guild.id);
 
     database_pool//Query to database to get code to execute
-    .query("SELECT code FROM commands WHERE server_id = $1 AND name = $2 AND active = TRUE LIMIT 1;", [CURRENT_GUILD.id, interaction.commandName])
+    .query("SELECT code, ephemeral FROM commands WHERE server_id = $1 AND name = $2 AND active = TRUE LIMIT 1;", [CURRENT_GUILD.id, interaction.commandName])
     .then(async (res)=>{
 
       if(res.rows.length!==1){
@@ -63,6 +63,9 @@ module.exports = {
       logger.debug("Got SQL result for "+CURRENT_GUILD.id+", we found a command to run !");
 
       const vm = get_sandbox({CURRENT_GUILD:CURRENT_GUILD, Discord:Discord, interaction:interaction});//A sandbox is created in module init_sandbox.js
+
+      //We will delay the answer and start the sandbox :
+      await interaction.deferReply({ ephemeral: res.rows[0].ephemeral });
       vm.run(globalVars+"async function a(){"+res.rows[0].code+"};a();");
 
     })
