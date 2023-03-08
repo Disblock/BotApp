8f3303ef9ee6894e126aa130e91d44c71ba2d24d
LJ50 <75009579+LJ5O@users.noreply.github.com>
2023-03-09 00:31:07 +0100

    Key and data have now a maximum size
    Will avoid some errors while executing code

--- modules/data_storage_functions.js
+++ modules/data_storage_functions.js
@@ -6,6 +6,18 @@ module.exports = {
 
   saveValueInStorage: async function(database_pool, logger, serverId, storageName, key, value){
     /* Save a new value in a server's storage */
+
+    //Key and value must be defined
+    if(!key || key=='' || !value || value=='')return;
+
+    //We will check that key and valud aren't bigger than 32 or 256 char
+    if(key.length > 32){
+      key = key.slice(0, 32);
+    }
+    if(value.toString().length > 255){
+      value = value.toString().slice(0, 255);
+    }
+
     database_pool.query("SELECT f_insert_or_update_data($1, $2, $3, $4)",
       [serverId, storageName, key, value.toString()])
     .catch((err)=>{
@@ -16,6 +28,12 @@ module.exports = {
 
   getValueInStorage: async function(database_pool, logger, serverId, storageName, key){
     /* Get a value stored in a data storage from database */
+
+    //We will check that key isn't bigger than 32 char
+    if(key.length > 32){
+      key = key.slice(0, 32);
+    }
+
     try{
       const row = (await database_pool.query("SELECT d.data, s.storage_is_int FROM data_storage s INNER JOIN stored_data d ON s.storage_id=d.storage_id WHERE s.server_id = $1 AND s.storage_name = $2 AND d.data_key = $3;",
         [serverId, storageName, key])).rows[0];
