e0889e43df53decf537b788e63bbc3617d9a89f6
LJ5O <75009579+LJ5O@users.noreply.github.com>
2023-08-04 16:08:51 +0200

    Moved globalVars in init_sandbox.js
    Also renamed that "initScript"
    Will be edited later to include everything required for the sandbox

--- modules/init_sandbox.js
+++ modules/init_sandbox.js
@@ -2,6 +2,35 @@
 const ivm = require('isolated-vm');
 
 module.exports = function(sandboxContext, args, database_pool, logger, serverId){
+  //Alls these var must be declared when executing generated code. These var are created at code generation ( Blockly ) Functions used by blocks can also be added here
+  initScript = `
+  let embedMessage, createdTextChannel, createdVoiceChannel, sentMessage, createdThreadOnMessage, createdRank;
+  let temporaryStorage = {};
+
+  /*Functions*/
+  function colourRandom() {
+      let num = Math.floor(Math.random() * Math.pow(2, 24));
+      return '#' + ('00000' + num.toString(16)).substr(-6);
+  }
+
+  function mathRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min)
+  };
+  const sleep = ms => new Promise(r => {
+      if (ms > 5000) {
+          throw ('Timeout too long !')
+      }
+      setTimeout(r, ms)
+  });
+
+  function strToInt(str) {
+      if (/^[0-9]{1,16}$/.test(str)) {
+          return (parseInt(str));
+      } else {
+          return (-1);
+      }
+  }
+  `;
 
   // This makes the global object available in the context as `global`. We use `derefInto()` here
   // because otherwise `global` would actually be a Reference{} object in the new isolate.
@@ -20,4 +49,6 @@ module.exports = function(sandboxContext, args, database_pool, logger, serverId)
   messagesFunctions.setSync('reply', new ivm.Reference(async(message, text)=>{
     args.eventMessage.reply(text);
   }));
+
+  return initScript;
 }
--- modules/sandbox.js
+++ modules/sandbox.js
@@ -3,13 +3,6 @@ const ivm = require('isolated-vm');//sandbox
 const data_storage_functions = require('./data_storage_functions.js');//Functions used to manage data saved in database
 const initSandbox = require('./init_sandbox.js');//Used to add functions and everything required to sandbox
 
-//Alls these var must be declared when executing generated code. These var are created at code generation ( Blockly ) Functions used by blocks can also be added here
-const globalVars = `let embedMessage,createdTextChannel,createdVoiceChannel,sentMessage,createdThreadOnMessage,createdRank;let temporaryStorage = {};
-  /*Functions*/ function colourRandom() {let num = Math.floor(Math.random() * Math.pow(2, 24));return '#' + ('00000' + num.toString(16)).substr(-6);}
-  function mathRandomInt(min, max){return Math.floor(Math.random() * (max - min + 1) + min)};
-  const sleep = ms => new Promise(r => {if(ms>5000){throw('Timeout too long !')}setTimeout(r, ms)});
-  function strToInt(str){if(/^[0-9]{1,16}$/.test(str)){return(parseInt(str));}else{return(-1);}}`;
-
 //Function that will add the object that allow us to manage stored data in database to sandbox
 function addDataStorageObject(jail, database_pool, logger, serverId){
   const dataStorage = {
@@ -44,10 +37,10 @@ module.exports = {
       jail.setSync(key, new ivm.Reference(args[key], {unsafeInherit:false}));//Adding old args. Here, only properties are kept. Methods aren't passed to isolate
     };
 
-    initSandbox(jail, args, database_pool, logger, args.CURRENT_GUILD.id);
+    const initScript = initSandbox(jail, args, database_pool, logger, args.CURRENT_GUILD.id);
 
     for(let i=0; i<sqlRows.length; i++){//For each row ( event block ), we execute the code
-      const code = await isolate.compileScript(globalVars+sqlRows[i].code);
+      const code = await isolate.compileScript(initScript+sqlRows[i].code);
       await code.run(context).catch(err => {
         logger.error("Error in sandbox : "+err);
       });
